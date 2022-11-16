648a1be5e3fde9a61d0eb7302bdbaa274a62053c
Alejandroleortiz <alejandroleortiz57@gmail.com>
2022-11-16 19:57:11 +0000

    first commit

--- src/app.js
+++ src/app.js
@@ -5,7 +5,32 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+window.onload = () => {
+  document.querySelector("#la-excusa").innerHTML = generateExcuse();
+  console.log("Hola Rigo");
+};
+let generateExcuse = () => {
+  let pronoun = ["El", "Un"];
+  let subject = ["perro", "gato", "loro", "policia", "cocodrilo", "perro"];
+  let action = ["robo", "tomo", " dejo", " desaparecio"];
+  let possetion = ["la llave", "mi celular", "mi cedula", "mi ropa"];
+  let where = ["en la calle", "en mi casa", "en el autobus"];
+
+  let proIndex = Math.floor(Math.random() * pronoun.length);
+  let subjIndx = Math.floor(Math.random() * subject.length);
+  let actionIndex = Math.floor(Math.random() * action.length);
+  let possetionIndex = Math.floor(Math.random() * possetion.length);
+  let whereIndex = Math.floor(Math.random() * where.length);
+
+  return (
+    pronoun[proIndex] +
+    " " +
+    subject[subjIndx] +
+    " " +
+    action[actionIndex] +
+    " " +
+    possetion[possetionIndex] +
+    " " +
+    where[whereIndex]
+  );
 };
